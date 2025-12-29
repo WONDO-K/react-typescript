ef0bb11eb823eca74fba869f4adbb27381f96e70
최동호 <raspic5874@naver.com>
2025-12-29 23:54:46 +0900

    [Fix] Props 관련 주석 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Hello from './Hello';
 
+// 디폴트가 아닌 이름 있는 내보내기(named export)이기 때문에 import 할 때 중괄호를 사용해야 한다.
+// import { Hello } from './Hello2';
+
 function App() {
-  return (
-      <Hello name={ "around hub studio" }></Hello>
-  );
+  return <Hello name='동호' color='blue' />;
 }
 
 export default App;
--- src/Hello.tsx
+++ src/Hello.tsx
@@ -11,4 +11,4 @@ export const Hello = ({ name, color = 'blue' }: Props) => {
 //     return <div style={{color}}>Hello {name}</div>;
 // }
 
-export default Hello;
+export default Hello; // 디폴트로 내보내기를 했기 때문에 import 할 때 중괄호 없이 불러올 수 있다.
